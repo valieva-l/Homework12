c1a34ca5f6bc2bc29b192b0634910141bda578d4
lyays <l28075339@gmail.com>
2025-04-04 18:59:56 +0300

    hm12

--- src/Author.java
+++ src/Author.java
@@ -1,12 +1,17 @@
 public class Author {
-    private String author;
+    private String authorName;
+    private String authorSurname;
 
-    public Author(String author) {
-        this.author = author;
+    public Author(String authorName, String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
     }
 
-    public String getAuthor() {
-        return this.author;
+    public String getAuthorName() {
+        return this.authorName;
+    }
+    public String getAuthorSurname() {
+        return this.authorSurname;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        Author detectiveAuthor = new Author("Артур Конан Дойл");
-        Author fantasyAuthor = new Author("Макс Фрай");
+        Author detectiveAuthor = new Author("Артур", "Конан Дойл");
+        Author fantasyAuthor = new Author("Макс", "Фрай");
         Book detectiveBook = new Book("Собака Баскервилей", 1901, detectiveAuthor);
 
-        System.out.println("detectiveAuthor.name = " + detectiveAuthor.getAuthor());
+        System.out.println("detectiveAuthor.name = " + detectiveAuthor.getAuthorName() + " " + detectiveAuthor.getAuthorSurname());
         System.out.println("detectiveBook.title = " + detectiveBook.getTitle());
         System.out.println("detectiveBook.Year = " + detectiveBook.getYear());
         detectiveBook.setYear(1902);
         System.out.println("detectiveBook.getYear() = " + detectiveBook.getYear());
 
         Book fantasyBook = new Book("Чужак", 1996, fantasyAuthor);
-        System.out.println("fantasyAuthor.name = " + fantasyAuthor.getAuthor());
+        System.out.println("fantasyAuthor.getAuthorName = " + fantasyAuthor.getAuthorName() + " " + fantasyAuthor.getAuthorSurname());
         System.out.println("fantasyBook.title = " + fantasyBook.getTitle());
         System.out.println("fantasyBook.Year = " + fantasyBook.getYear());
 
